243b3908d8ed81a9a9d9857373e1f42af0646c89
Rijas Afradi <rijrijas@gmail.com>
2025-01-20 19:59:43 +0530

    signup updated

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -22,79 +22,39 @@ const getUsers = async (req, res, next) => {
 const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(
-      new HttpError("Invalid inputs passed, please check your data.", 422)
-    );
+    return res.status(422).json({ message: "Invalid inputs passed" });
   }
-
-  const { name, email, password } = req.body;
-
-  let existingUser;
-  try {
-    existingUser = await User.findOne({ email: email });
-  } catch (err) {
-    const error = new HttpError(
-      "Signing up failed, please try again later.",
-      500
-    );
-    return next(error);
-  }
-
-  if (existingUser) {
-    const error = new HttpError(
-      "User exists already, please login instead.",
-      422
-    );
-    return next(error);
-  }
-
-  let hashedPassword;
-  try {
-    hashedPassword = await bcrypt.hash(password, 12);
-  } catch (err) {
-    const error = new HttpError(
-      "Could not create user, please try again.",
-      500
-    );
-    return next(error);
-  }
-
-  const createdUser = new User({
-    name,
-    email,
-    image: req.file.path,
-    password: hashedPassword,
-    places: [],
-  });
 
   try {
+    const { name, email, password } = req.body;
+    let existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      return res.status(422).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const createdUser = new User({
+      name,
+      email,
+      image: req.file.path,
+      password: hashedPassword,
+      places: [],
+    });
+
     await createdUser.save();
-  } catch (err) {
-    const error = new HttpError(
-      "Signing up failed, please try again later.",
-      500
-    );
-    return next(error);
-  }
 
-  let token;
-  try {
-    token = jwt.sign(
+    const token = jwt.sign(
       { userId: createdUser.id, email: createdUser.email },
       process.env.JWT_KEY,
       { expiresIn: "1h" }
     );
+    res
+      .status(201)
+      .json({ userId: createdUser.id, email: createdUser.email, token: token });
   } catch (err) {
-    const error = new HttpError(
-      "Signing up failed, please try again later.",
-      500
-    );
-    return next(error);
+    return res.status(500).json({ message: "Error creating user" });
   }
-
-  res
-    .status(201)
-    .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
