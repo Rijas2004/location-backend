80644fe4b58a9416af7274934c6a85baea6f122d
Rijas Afradi <rijrijas@gmail.com>
2025-01-20 23:16:07 +0530

    user.js updated

--- models/user.js
+++ models/user.js
@@ -1,16 +1,40 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 6 },
-  image: { type: String, required: true },
-  places: [{ type: mongoose.Types.ObjectId, required: true, ref: "Place" }],
-});
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please provide a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    image: { type: String, required: true },
+    places: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Place",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-userSchema.plugin(uniqueValidator);
+userSchema.path("email").validate(async function (value) {
+  const emailCount = await mongoose.models.User.countDocuments({
+    email: value,
+  });
+  return emailCount === 0;
+}, "Email already exists");
 
 module.exports = mongoose.model("User", userSchema);
