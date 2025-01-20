b2b4c203e8ffa2874a235685f0b9e44b6d0e5f7a
Rijas Afradi <rijrijas@gmail.com>
2025-01-20 22:45:03 +0530

    vercel.json updated 4

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(
   })
 );
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
